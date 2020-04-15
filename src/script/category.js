d5362d80aac558cadf1dc6e35290808160862363
Yahor Neverouski <hardstyle6537@gmail.com>
2020-04-16 00:14:10 +0300

    feat: add card generator for main-page

---
+++ src/script/category.js
@@ -0,0 +1,22 @@
+class Category {
+  constructor() {
+    this.mainPage = document.querySelector('.main-page');
+    this.categoryImages = ['Actions', 'Animals (set 1)', 'Animals (set 2)', 'Clothes', 'Emotions', 'Fruits', 'Sweets', 'Vegetables'];
+  }
+
+  elementGeneration() {
+    const categoryCard = document.querySelector('.category-card');
+    for (let i = 1; i < this.categoryImages.length; i += 1) {
+      const newElem = categoryCard.cloneNode(true);
+      newElem.firstElementChild.alt = this.categoryImages[i];
+      newElem.firstElementChild.src = `images/category/${this.categoryImages[i]}.jpg`;
+      newElem.lastElementChild.textContent = this.categoryImages[i];
+      this.mainPage.append(newElem);
+    }
+  }
+}
+
+const category = new Category();
+category.elementGeneration();
+
+export default category;
