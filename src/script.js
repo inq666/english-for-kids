1e9a5c74acfbdeae552d6bab5b06eddf4480c29a
Yahor Neverouski <hardstyle6537@gmail.com>
2020-04-19 00:38:12 +0300

    feat: add statistics, function sort

--- src/script.js
+++ src/script.js
@@ -1,30 +1,43 @@
-import menuy from './script/menu.js';
+import menu from './script/menu.js';
 import category from './script/category.js';
 import cards from './script/cards.js';
 import switcher from './script/switcher.js';
 
 class Game {
   constructor() {
+    this.statisticsWord = JSON.parse(localStorage.getItem('statistics'));
+    this.categoryImages = category.categoryImages;
     this.mainPage = category.mainPage;
     this.category = category.categoryImages;
     this.cardData = cards.cardData;
     this.cardsWrapper = cards.cardsWrapper;
     this.startGameButton = switcher.startGame;
+    this.nav = menu.nav;
     this.gameMode = false;
     this.audio = new Audio();
     this.audio.autoplay = true;
+    this.sortMode = false;
   }
 
   createDOM() {
+    if (this.statisticsWord === null) {
+      this.statisticsReset();
+    }
+    this.table = document.querySelector('table');
+    this.statistics = document.querySelector('.statistics');
     this.wrapper = document.querySelector('.wrapper');
     this.container = document.querySelector('.container');
     this.cardsPage = document.querySelector('.cards-page');
     this.repeatButton = document.querySelector('.repeat');
     this.stars = document.querySelector('.stars');
+    this.resetButton = document.querySelector('.reset-statistics');
     this.cardsPage.remove();
+    this.statistics.remove();
   }
 
   eventListener() {
+    this.table.addEventListener('click', (event) => this.statisticsSort(event));
+    this.resetButton.addEventListener('click', () => this.statisticsReset());
     this.wrapper.addEventListener('click', (event) => this.checkClickLink(event));
     this.container.addEventListener('click', (event) => this.rotateCard(event));
     this.container.addEventListener('mousemove', (event) => this.reverseRotateCard(event));
@@ -32,6 +45,9 @@ class Game {
     this.startGameButton.addEventListener('click', () => this.startGame());
     this.cardsPage.addEventListener('click', (event) => this.playGame(event));
     this.repeatButton.addEventListener('click', () => this.newWord());
+    window.addEventListener('unload', () => {
+      localStorage.setItem('statistics', JSON.stringify(this.statisticsWord));
+    });
   }
 
   startGame() {
@@ -40,10 +56,10 @@ class Game {
     this.repeatButton.style.display = 'block';
     const index = this.category.indexOf(this.currentPage);
     this.randomArray = this.cardData[index].sort(() => Math.random() - 0.5);
-    this.errors = 0;
+    this.mistakes = 0;
     this.nextElem = 0;
     this.gameMode = true;
-    setTimeout(() => this.newWord(), 400);
+    this.newWord();
   }
 
   playGame(event) {
@@ -55,6 +71,7 @@ class Game {
       newStar.classList.add('star-win');
       useCard.classList.add('correctly-word');
       this.nextElem += 1;
+      this.statisticsWord[this.currentWord].correct += 1;
       this.audio.src = 'audio/correct.mp3';
       if (this.nextElem === 8) {
         setTimeout(() => this.finishGame(), 600);
@@ -63,7 +80,8 @@ class Game {
       setTimeout(() => this.newWord(), 600);
     } else {
       newStar.classList.add('star-lose');
-      this.errors += 1;
+      this.mistakes += 1;
+      this.statisticsWord[this.currentWord].error += 1;
       this.audio.src = 'audio/error.mp3';
     }
     this.stars.prepend(newStar);
@@ -75,10 +93,10 @@ class Game {
   }
 
   finishGame() {
-    if (this.errors) {
+    if (this.mistakes) {
       this.audio.src = 'audio/sad_finish.mp3';
       document.querySelector('.lose-game').style.display = 'block';
-      document.querySelector('.lose-game p').textContent = `Errors: ${this.errors}`;
+      document.querySelector('.lose-game p').textContent = `Mistakes: ${this.mistakes}`;
     } else {
       this.audio.src = 'audio/success.mp3';
       document.querySelector('.win-game').style.display = 'block';
@@ -94,7 +112,7 @@ class Game {
   stopGame() {
     this.cardsPage.querySelector('.start-game').style.display = 'block';
     this.cardsPage.querySelectorAll('.correctly-word').forEach((item) => item.classList.remove('correctly-word'));
-    this.errors = 0;
+    this.mistakes = 0;
     this.nextElem = 0;
     this.gameMode = false;
     this.repeatButton.style.display = 'none';
@@ -113,7 +131,7 @@ class Game {
     if (clickLink === null) return;
     this.currentPage = clickLink.getAttribute('href').slice(1);
     this.highlightingLink();
-    if (this.currentPage === 'MainPage') return;
+    if (this.currentPage === 'MainPage' || this.currentPage === 'Statistics') return;
     this.createCardsPage();
   }
 
@@ -125,9 +143,16 @@ class Game {
       }
     });
     if (this.currentPage === 'MainPage') {
-      document.querySelector('.nav-link').classList.add('active-link');
+      this.nav.firstElementChild.classList.add('active-link');
       this.container.append(this.mainPage);
       this.cardsPage.remove();
+      this.statistics.remove();
+    }
+    if (this.currentPage === 'Statistics') {
+      document.body.append(this.statistics);
+      this.cardsPage.remove();
+      this.mainPage.remove();
+      this.generatorStatistics();
     }
   }
 
@@ -135,6 +160,7 @@ class Game {
     if (this.gameMode) {
       this.stopGame();
     }
+    this.statistics.remove();
     this.mainPage.remove();
     this.container.append(this.cardsPage);
     const index = this.category.indexOf(this.currentPage);
@@ -152,6 +178,7 @@ class Game {
     const pressCard = event.target.closest('.word-card');
     if (pressCard === null) return;
     const currentWord = pressCard.querySelector('.word-control-front .card-title').textContent;
+    this.statisticsWord[currentWord].click += 1;
     this.audio.src = `audio/${currentWord}.mp3`;
   }
 
@@ -172,7 +199,7 @@ class Game {
   reverseRotateCard(event) {
     if (this.currentCard) {
       if (this.rotateMode) {
-        if (event.toElement.classList.contains('cards-wrapper')) {
+        if (!event.toElement.classList.contains('word-card')) {
           this.currentCard.querySelector('.word-control-back').style.transform = 'rotateY(180deg)';
           this.currentCard.querySelector('.word-control-front').style.transform = 'rotateY(0deg)';
           this.currentCard.style.transform = 'rotateY(0deg)';
@@ -180,6 +207,108 @@ class Game {
           this.rotateMode = false;
         }
       }
+    }
+  }
+
+  generatorStatistics() {
+    const tableContainer = document.querySelector('tbody');
+    tableContainer.innerHTML = '';
+    for (let i = 0; i < this.cardData.length; i += 1) {
+      for (let z = 0; z < this.cardData[i].length; z += 1) {
+        const row = document.createElement('tr');
+        const categoryWord = document.createElement('td');
+        const word = document.createElement('td');
+        const translate = document.createElement('td');
+        const clickTrain = document.createElement('td');
+        const rightAnswer = document.createElement('td');
+        const mistakesInGame = document.createElement('td');
+        const percentMistakes = document.createElement('td');
+        categoryWord.innerHTML = this.categoryImages[i];
+        word.innerHTML = this.cardData[i][z].word;
+        translate.innerHTML = this.cardData[i][z].translation;
+        clickTrain.innerHTML = this.statisticsWord[this.cardData[i][z].word].click;
+        rightAnswer.innerHTML = this.statisticsWord[this.cardData[i][z].word].correct;
+        mistakesInGame.innerHTML = this.statisticsWord[this.cardData[i][z].word].error;
+        percentMistakes.innerHTML = `${((this.statisticsWord[this.cardData[i][z].word].error
+          / (this.statisticsWord[this.cardData[i][z].word].error + this.statisticsWord[this.cardData[i][z].word].correct)) * 100).toFixed(2)}%`;
+        if (percentMistakes.textContent === 'NaN%') {
+          percentMistakes.textContent = '0.00%';
+        }
+        row.append(categoryWord);
+        row.append(word);
+        row.append(translate);
+        row.append(clickTrain);
+        row.append(rightAnswer);
+        row.append(mistakesInGame);
+        row.append(percentMistakes);
+        tableContainer.append(row);
+      }
+    }
+  }
+
+  statisticsReset() {
+    this.statisticsWord = {};
+    for (let i = 0; i < this.cardData.length; i += 1) {
+      for (let z = 0; z < this.cardData[i].length; z += 1) {
+        this.statisticsWord[this.cardData[i][z].word] = {
+          error: 0,
+          correct: 0,
+          click: 0,
+        };
+      }
+    }
+    this.generatorStatistics();
+  }
+
+  statisticsSort(event) {
+    if (event.target.tagName !== 'TH') return;
+    const columnNumber = event.toElement.cellIndex;
+    this.allRow = document.querySelectorAll('tr');
+    if (columnNumber < 3) {
+      this.sortWord(columnNumber);
+    } else {
+      this.sortNum(columnNumber);
+    }
+    if (this.sortMode) {
+      this.sortMode = false;
+    } else {
+      this.sortMode = true;
+    }
+  }
+
+  sortWord(columnNumber) {
+    for (let i = 1; i < this.allRow.length - 1; i += 1) {
+      let min = i;
+      for (let j = i + 1; j < this.allRow.length; j += 1) {
+        const minRow = this.allRow[min].children[columnNumber].textContent;
+        const nextRow = this.allRow[j].children[columnNumber].textContent;
+        if (nextRow < minRow && !this.sortMode) {
+          min = j;
+        } else if (nextRow > minRow && this.sortMode) {
+          min = j;
+        }
+      }
+      const dummy = this.allRow[i].innerHTML;
+      this.allRow[i].innerHTML = this.allRow[min].innerHTML;
+      this.allRow[min].innerHTML = dummy;
+    }
+  }
+
+  sortNum(columnNumber) {
+    for (let i = 1; i < this.allRow.length - 1; i += 1) {
+      let min = i;
+      for (let j = i + 1; j < this.allRow.length; j += 1) {
+        const minRow = parseFloat(this.allRow[min].children[columnNumber].textContent);
+        const nextRow = parseFloat(this.allRow[j].children[columnNumber].textContent);
+        if (nextRow > minRow && !this.sortMode) {
+          min = j;
+        } else if (nextRow < minRow && this.sortMode) {
+          min = j;
+        }
+      }
+      const dummy = this.allRow[i].innerHTML;
+      this.allRow[i].innerHTML = this.allRow[min].innerHTML;
+      this.allRow[min].innerHTML = dummy;
     }
   }
 }
